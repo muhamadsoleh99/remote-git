ffaff1231b679a6d7b0b4b76c31ae775c45256c6
muhamadsoleh99 <muhamad.soleh@iti.ac.id>
2024-07-02 07:26:48 +0700

    menambahkan paraghraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati fugiat enim nisi aliquam optio quo, voluptates ratione. Est repudiandae mollitia ipsum odio, corrupti aspernatur, ducimus, perspiciatis nobis quis quo voluptatum?</p>
 </body>
 </html>
